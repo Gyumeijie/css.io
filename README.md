627dc6682ca0e6db77c2d317bd9522b74030e2f3
Gyumeijie <18170879819@163.com>
2019-05-23 13:03:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# css.io
+
+A curated collection of useful CSS snippets. Inspired by [30 seconds of code](https://github.com/Chalarangelo/30-seconds-of-code).
+
+## View online
+
+https://Gyumeijie.github.io/code-snippets.CSS
+
+## License
+MIT © [Gyumeijie](https://github.com/Gyumeijie)
---
+++ src/js/components/BackToTopButton.js
@@ -0,0 +1,15 @@
+import jump from 'jump.js';
+import { select, scrollY, easeOutQuint } from '../deps/utils';
+
+const backToTopButton = select('.back-to-top-button');
+
+window.addEventListener('scroll', () => {
+  backToTopButton.classList[scrollY() > 500 ? 'add' : 'remove']('is-visible');
+});
+
+backToTopButton.onclick = () => {
+  jump('.header', {
+    duration: 750,
+    easing: easeOutQuint
+  });
+};
